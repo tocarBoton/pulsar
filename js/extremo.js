f1a5fcd685ad37ccd4427f1bfd45ecc97536a0c5
tocarBoton <69401319+tocarBoton@users.noreply.github.com>
2020-08-08 16:39:39 -0300

    Add files via upload

---
+++ js/extremo.js
@@ -0,0 +1,5 @@
+document.querySelector("#boton-extremo").onclick = function () {
+	document.querySelector("#imagen").className = "1";
+	document.querySelector("#boton-extremo").className = "oculto";
+	document.querySelector("#texto").className = "1";
+};
